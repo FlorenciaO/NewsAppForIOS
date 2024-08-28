743194dff4af235ee44a368d2c96c7becb441f8e
Florencia Olivera <folivera@carnival.com>
2024-08-28 16:36:51 -0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Florencia Olivera on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
